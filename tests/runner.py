65385f8ea37689b369adf152414207bc7ec111b5
Krishna Muddi <krishna@antstack.io>
2023-03-17 18:38:38 +0530

    rerun action

--- tests/runner.py
+++ tests/runner.py
@@ -18,7 +18,6 @@ print(os.getcwd())
 # Skip writing pyc files on a readonly filesystem.
 sys.dont_write_bytecode = True
 
-# Unit test.
 # Run pytest.
 retcode = pytest.main([".", "-v", "-p", "no:cacheprovider", "--cov","--cov-report", "xml"])
 
