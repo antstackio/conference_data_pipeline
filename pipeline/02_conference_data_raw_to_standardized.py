fbefef82be2c4e94d287041a058187d53ab2ff32
sourav.mondal@antstack.io <sourav.mondal@antstack.io>
2023-03-23 08:04:47 +0000

    SCD-62 Add cast string to timestamp

--- pipeline/02_conference_data_raw_to_standardized.py
+++ pipeline/02_conference_data_raw_to_standardized.py
@@ -1,5 +1,13 @@
 # Databricks notebook source
-from src.main import convert_date_object
+from src.main import (
+    convert_date_object,
+    read_data_from_raw,
+    add_attendee_type,
+    create_timestamp_str,
+    select_columns_from_dataframe,
+    union_dataframes,
+)
+from pyspark.sql import SparkSession
 from pyspark.sql.types import StringType
 from pyspark.sql.functions import (
     lit,
@@ -12,7 +20,13 @@ from pyspark.sql.functions import (
     current_date,
     col,
     cast,
+    concat,
+    to_timestamp,
 )
+
+# COMMAND ----------
+
+spark = SparkSession.builder.getOrCreate()
 
 # COMMAND ----------
 
@@ -26,15 +40,7 @@ from pyspark.sql.functions import (
 
 # COMMAND ----------
 
-def read_data_from_raw(schema:str, table_name: str) -> DataFrame:
-    df = spark.read.table(f"{schema}.{table_name}")
-    df = df.filter(df.is_processed == 'false')
-    df.printSchema()
-    return df
-
-# COMMAND ----------
-
-event_df = read_data_from_raw('conference_raw', 'event')
+event_df = read_data_from_raw(spark, "conference_raw", "event")
 
 # COMMAND ----------
 
@@ -43,7 +49,7 @@ event_df = read_data_from_raw('conference_raw', 'event')
 
 # COMMAND ----------
 
-session_df = read_data_from_raw('conference_raw', 'session')
+session_df = read_data_from_raw(spark, "conference_raw", "session")
 
 # COMMAND ----------
 
@@ -52,13 +58,7 @@ session_df = read_data_from_raw('conference_raw', 'session')
 
 # COMMAND ----------
 
-def add_attendee_type(df: DataFrame, attendee_type: str) -> DataFrame:
-    result_df = df.withColumn('attendee_type', lit(attendee_type))
-    return result_df
-
-# COMMAND ----------
-
-inperson_attendee_df = read_data_from_raw('conference_raw', 'in_person_attendee')
+inperson_attendee_df = read_data_from_raw(spark, "conference_raw", "in_person_attendee")
 
 # COMMAND ----------
 
@@ -67,7 +67,7 @@ inperson_attendee_df = read_data_from_raw('conference_raw', 'in_person_attendee'
 
 # COMMAND ----------
 
-virtual_attendee_df = read_data_from_raw('conference_raw', 'virtual_attendee')
+virtual_attendee_df = read_data_from_raw(spark, "conference_raw", "virtual_attendee")
 
 # COMMAND ----------
 
@@ -76,7 +76,7 @@ virtual_attendee_df = read_data_from_raw('conference_raw', 'virtual_attendee')
 
 # COMMAND ----------
 
-polling_questions_df = read_data_from_raw('conference_raw', 'polling_questions')
+polling_questions_df = read_data_from_raw(spark, "conference_raw", "polling_questions")
 
 # COMMAND ----------
 
@@ -115,52 +115,149 @@ display(session_df)
 
 # COMMAND ----------
 
-inperson_attendee_df = add_attendee_type(inperson_attendee_df, 'inperson')
+inperson_attendee_df = add_attendee_type(inperson_attendee_df, "inperson")
 
 # COMMAND ----------
 
-inperson_attendee_df = inperson_attendee_df.withColumn('login_time', lit(None).cast(StringType()))
+inperson_attendee_df = inperson_attendee_df.withColumn(
+    "login_time", lit(None).cast(StringType())
+)
 
 # COMMAND ----------
 
-inperson_attendee_df = inperson_attendee_df.withColumn('logout_time', lit(None).cast(StringType()))
+inperson_attendee_df = inperson_attendee_df.withColumn(
+    "logout_time", lit(None).cast(StringType())
+)
 
 # COMMAND ----------
 
-attendee_columns = ['registration_no', 'first_name', 'last_name', 'email_address', 'job_role', 'state', 'session_title', 'attendee_type', 'login_time', 'logout_time']
-inperson_attendee_df = inperson_attendee_df.select(*attendee_columns)
-inperson_attendee_df.printSchema()
+virtual_registants_df = virtual_attendee_df.filter(
+    virtual_attendee_df.login_time.isNull() & virtual_attendee_df.logout_time.isNull()
+)
 
 # COMMAND ----------
 
-inperson_attendee_df = inperson_attendee_df.select(*attendee_columns)
-inperson_attendee_df.printSchema()
+virtual_registants_df.printSchema()
 
 # COMMAND ----------
 
-virtual_attendee_df = add_attendee_type(virtual_attendee_df, 'virtual')
+print(virtual_registants_df.count())
 
 # COMMAND ----------
 
-virtual_attendee_df = virtual_attendee_df.select(*attendee_columns)
+virtual_attendee_df = virtual_attendee_df.filter(
+    virtual_attendee_df.login_time.isNotNull()
+    & virtual_attendee_df.logout_time.isNotNull()
+).withColumnRenamed("session_title", "session")
+
+# COMMAND ----------
+
 virtual_attendee_df.printSchema()
 
 # COMMAND ----------
 
-print('Inperson attendee count', inperson_attendee_df.count(), sep=' :: ')
-print('Virtual attendee count', virtual_attendee_df.count(), sep=' :: ')
+virtual_attendee_df = virtual_attendee_df.join(
+    session_df,
+    virtual_attendee_df.session == session_df.session_title,
+    "left",
+).select(
+    virtual_attendee_df.registration_no,
+    virtual_attendee_df.first_name,
+    virtual_attendee_df.last_name,
+    virtual_attendee_df.email_address,
+    virtual_attendee_df.job_role,
+    virtual_attendee_df.state,
+    virtual_attendee_df.session,
+    virtual_attendee_df.login_time,
+    virtual_attendee_df.logout_time,
+    session_df.session_date,
+)
 
 # COMMAND ----------
 
-attendee = inperson_attendee_df.union(virtual_attendee_df)
+virtual_attendee_df = create_timestamp_str(virtual_attendee_df, 'session_date', 'login_time', 'login_time_')
+display(virtual_attendee_df.head(5))
 
 # COMMAND ----------
 
-attendee.printSchema()
+virtual_attendee_df = create_timestamp_str(virtual_attendee_df, 'session_date', 'logout_time', 'logout_time_')
+display(virtual_attendee_df.head(5))
 
 # COMMAND ----------
 
-print('Total attendee count', attendee.count(), sep=' :: ')
+v_df = virtual_attendee_df.withColumn(
+    "login_time_", to_timestamp(virtual_attendee_df.login_time_)
+).withColumn("logout_time_", to_timestamp(virtual_attendee_df.logout_time_))
+
+display(v_df)
+
+# COMMAND ----------
+
+virtual_attendee_df = v_df.drop("login_time", "logout_time")
+virtual_attendee_df.printSchema()
+
+# COMMAND ----------
+
+virtual_attendee_df = (
+    virtual_attendee_df.withColumnRenamed("login_time_", "login_time")
+    .withColumnRenamed("logout_time_", "logout_time")
+    .withColumnRenamed("session", "session_title")
+)
+virtual_attendee_df.printSchema()
+
+# COMMAND ----------
+
+attendee_columns = [
+    "registration_no",
+    "first_name",
+    "last_name",
+    "email_address",
+    "job_role",
+    "state",
+    "session_title",
+    "attendee_type",
+    "login_time",
+    "logout_time",
+]
+
+inperson_attendee_df = select_columns_from_dataframe(inperson_attendee_df, attendee_columns)
+
+# COMMAND ----------
+
+print('In person attendee count', inperson_attendee_df.count(), sep=" :: ")
+print('virtual attendee count', virtual_attendee_df.count(), sep=" :: ")
+print('virtual registrants count', virtual_registants_df.count(), sep=" :: ")
+
+# COMMAND ----------
+
+virtual_registants_df = add_attendee_type(virtual_registants_df, 'virtual')
+
+virtual_registants_df = select_columns_from_dataframe(virtual_registants_df, attendee_columns)
+
+# COMMAND ----------
+
+virtual_attendee_df = add_attendee_type(virtual_attendee_df, "virtual")
+
+# COMMAND ----------
+
+virtual_attendee_df = select_columns_from_dataframe(virtual_attendee_df, attendee_columns)
+
+# COMMAND ----------
+
+virtual_attendee_df = union_dataframes(virtual_attendee_df, virtual_registants_df)
+
+# COMMAND ----------
+
+print("Inperson attendee count", inperson_attendee_df.count(), sep=" :: ")
+print("Virtual attendee count", virtual_attendee_df.count(), sep=" :: ")
+
+# COMMAND ----------
+
+attendee = union_dataframes(inperson_attendee_df, virtual_attendee_df)
+
+# COMMAND ----------
+
+print("Total attendee count", attendee.count(), sep=" :: ")
 
 # COMMAND ----------
 
@@ -173,29 +270,29 @@ import os
 
 # COMMAND ----------
 
-os.listdir('../SqlDBM/src/Tables/')
+os.listdir("../SqlDBM/src/Tables/")
 
 # COMMAND ----------
 
-with open('../SqlDBM/src/Tables/conference_refined.event.sql') as file:
+with open("../SqlDBM/src/Tables/conference_refined.event.sql") as file:
     ddl = file.read()
     spark.sql(ddl)
 
 # COMMAND ----------
 
-with open('../SqlDBM/src/Tables/conference_refined.session.sql') as file:
+with open("../SqlDBM/src/Tables/conference_refined.session.sql") as file:
     ddl = file.read()
     spark.sql(ddl)
 
 # COMMAND ----------
 
-with open('../SqlDBM/src/Tables/conference_refined.registrant.sql') as file:
+with open("../SqlDBM/src/Tables/conference_refined.registrant.sql") as file:
     ddl = file.read()
     spark.sql(ddl)
 
 # COMMAND ----------
 
-with open('../SqlDBM/src/Tables/conference_refined.polling_questions.sql') as file:
+with open("../SqlDBM/src/Tables/conference_refined.polling_questions.sql") as file:
     ddl = file.read()
     spark.sql(ddl)
 
