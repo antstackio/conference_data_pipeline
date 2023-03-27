3587880bdf800535d1dddcb4cf6f97d91e5392ba
ropoojary18 <ropoojary18>
2023-03-27 08:34:07 +0000

    SCD-36 added prod creds and ddl pushdown

---
+++ pipeline/04_snowflake_ddl.py
@@ -0,0 +1,80 @@
+# Databricks notebook source
+from src.snowflake_credentials import *
+
+sfUtils = spark.sparkContext._jvm.net.snowflake.spark.snowflake.Utils
+
+# COMMAND ----------
+
+options = snowflake_options('trusted')
+
+# COMMAND ----------
+
+drop_query = [
+"drop table if exists CONFORMED.ATTENDEE_SESSION_DIM;",
+"drop table if exists CONFORMED.EVENT_ATTENDEE_FACT;",
+"drop table if exists CONFORMED.EVENT_DIM;",
+"drop table if exists CONFORMED.EVENT_REGISTRANT_DIM;",
+"drop table if exists CONFORMED.QUESTION_DIM;",
+"drop table if exists CONFORMED.QUESTION_ATTENDEE_DIM;",
+"drop table if exists CONFORMED.SATISFACTION_RATING;",
+"drop table if exists CONFORMED.SESSION_DIM;",
+"drop table if exists CONFORMED.ATTENDEE_SESSION_FACT",
+"drop table if exists CONFORMED.SESSION_POLL_FACT"
+]
+
+for query in drop_query:
+    sfUtils.runQuery(options, query)
+
+# COMMAND ----------
+
+with open("../SqlDBM/src/Tables/conformed.event_dim.sql") as file:
+    ddl = file.read()
+    sfUtils.runQuery(options, ddl)
+
+# COMMAND ----------
+
+with open("../SqlDBM/src/Tables/conformed.event_attendee_fact.sql") as file:
+    ddl = file.read()
+    sfUtils.runQuery(options, ddl)
+
+# COMMAND ----------
+
+with open("../SqlDBM/src/Tables/conformed.question_dim.sql") as file:
+    ddl = file.read()
+    sfUtils.runQuery(options, ddl)
+
+# COMMAND ----------
+
+with open("../SqlDBM/src/Tables/conformed.session_dim.sql") as file:
+    ddl = file.read()
+    sfUtils.runQuery(options, ddl)
+
+# COMMAND ----------
+
+with open("../SqlDBM/src/Tables/conformed.session_poll_fact.sql") as file:
+    ddl = file.read()
+    sfUtils.runQuery(options, ddl)
+
+# COMMAND ----------
+
+with open("../SqlDBM/src/Tables/conformed.attendee_session_dim.sql") as file:
+    ddl = file.read()
+    sfUtils.runQuery(options, ddl)
+
+# COMMAND ----------
+
+with open("../SqlDBM/src/Tables/conformed.attendee_session_fact.sql") as file:
+    ddl = file.read()
+    sfUtils.runQuery(options, ddl)
+
+# COMMAND ----------
+
+with open("../SqlDBM/src/Tables/conformed.event_registrant_dim.sql") as file:
+    ddl = file.read()
+    sfUtils.runQuery(options, ddl)
+
+# COMMAND ----------
+
+with open("../SqlDBM/src/Tables/conformed.question_attendee_dim.sql") as file:
+    ddl = file.read()
+    sfUtils.runQuery(options, ddl)
--- src/snowflake_credentials.py
+++ src/snowflake_credentials.py
@@ -1,14 +1,24 @@
+import os
 from pyspark.sql import SparkSession
 from pyspark.dbutils import DBUtils
 
 spark = SparkSession.builder.getOrCreate()
 dbutils = DBUtils(spark)
 
-sfDatabase = "ANTSTACK_DEMO_DATABASE_DEV"
-sfUser = dbutils.secrets.get("snowflake-secrets", "user-name")
-sfPassword = dbutils.secrets.get("snowflake-secrets", "password")  
-sfRole = "ANTSTACK_DEMO_ROLE_DEV"
-sfWarehouse = "ANTSTACK_DEMO_DEV"
+dbc_env = os.getenv("dbc_environment")
+
+if dbc_env == "dev" or dbc_env is None:
+    sfDatabase = "ANTSTACK_DEMO_DATABASE_DEV"
+    sfUser = dbutils.secrets.get("snowflake-secrets", "user-name")
+    sfPassword = dbutils.secrets.get("snowflake-secrets", "password")  
+    sfRole = "ANTSTACK_DEMO_ROLE_DEV"
+    sfWarehouse = "ANTSTACK_DEMO_DEV"
+else:
+    sfDatabase = "ANTSTACK_DEMO_DATABASE_PROD"
+    sfUser = dbutils.secrets.get("snowflake-secrets", "user-name-prod")
+    sfPassword = dbutils.secrets.get("snowflake-secrets", "password-prod")  
+    sfRole = "ANTSTACK_DEMO_ROLE_PROD"
+    sfWarehouse = "ANTSTACK_DEMO_PROD"
 
 def snowflake_options(env):
     schema = "CORE" if env == 'raw' else "CONFORMED"
