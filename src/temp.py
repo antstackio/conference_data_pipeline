bca9bd61bec23a335ecc50d8a07da934c7fdc4ae
Krishna Muddi <krishna@antstack.io>
2023-03-27 13:23:59 +0530

    added duplicate function

--- src/temp.py
+++ src/temp.py
@@ -17,3 +17,23 @@ def process_data(data):
                 continue
     else:
         return
+
+def duplicate_function(data):
+    if isinstance(data, dict):
+        for key, value in data.items():
+            if isinstance(value, list):
+                for item in value:
+                    if isinstance(item, dict):
+                        process_data(item)
+            elif isinstance(value, str):
+                print(value)
+            else:
+                continue
+    elif isinstance(data, list):
+        for item in data:
+            if isinstance(item, dict):
+                process_data(item)
+            else:
+                continue
+    else:
+        return
