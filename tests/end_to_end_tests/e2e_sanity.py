56a9b7b15254061841080b34ebfc6bf41b0f9dea
binums <binums>
2023-04-06 12:13:09 +0000

    Feat: add the job json for the whole pipeline

--- tests/end_to_end_tests/e2e_sanity.py
+++ tests/end_to_end_tests/e2e_sanity.py
@@ -7,7 +7,8 @@ import os
 
 # COMMAND ----------
 
-dbc_env = os.getenv("dbc_environment")
+# dbc_env = os.getenv("dbc_environment")
+dbc_env = "stage"
 
 if dbc_env == "dev" or dbc_env is None:
     # dev src file path
@@ -21,8 +22,13 @@ else:
     bucket = "conference-data-ap-south-1-landing-prod/"
     src_file_path = bucket + 'input/'
 
+
 # COMMAND ----------
 
-mvCommand = f'aws s3 mv s3://{bucket}processed/ s3://{src_file_path} --recursive --acl bucket-owner-full-control'
-print(mvCommand)
-# resp = os.system(mvCommand)
+try:
+    mvCommand = f'aws s3 mv s3://{bucket}processed/ s3://{src_file_path} --recursive --acl bucket-owner-full-control'
+    print(mvCommand)
+    resp = os.system(mvCommand)
+    print(resp)
+except Exception as e:
+    print(e)
