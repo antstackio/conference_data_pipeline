35211702a4e193cc0023685bf0427697b852f650
binums <binums>
2023-04-10 07:10:35 +0000

    Fix: On time delete tables and databases at the beginning of e2e job

--- tests/end_to_end_tests/e2e_pre_requisites.py
+++ tests/end_to_end_tests/e2e_pre_requisites.py
@@ -1,6 +1,28 @@
 # Databricks notebook source
 # MAGIC %sql
 # MAGIC 
+# MAGIC DELETE DATABASE conference_raw CASCADE
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC 
+# MAGIC DELETE DATABASE conference_refined CASCADE
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC 
+# MAGIC DELETE DATABASE conference_trusted CASCADE
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC 
 # MAGIC CREATE DATABASE IF NOT EXISTS conference_raw
 
 # COMMAND ----------
