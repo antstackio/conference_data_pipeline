df9f1e2ffd036b0ed15fd9550e8d81d43d68a556
binums <binums>
2023-04-10 07:37:44 +0000

    Fix: remove temp drop database

--- tests/end_to_end_tests/e2e_pre_requisites.py
+++ tests/end_to_end_tests/e2e_pre_requisites.py
@@ -1,28 +1,6 @@
 # Databricks notebook source
 # MAGIC %sql
 # MAGIC 
-# MAGIC DROP DATABASE conference_raw CASCADE
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC 
-# MAGIC DROP DATABASE conference_refined CASCADE
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC 
-# MAGIC DROP DATABASE conference_trusted CASCADE
-
-# COMMAND ----------
-
-
-
-# COMMAND ----------
-
-# MAGIC %sql
-# MAGIC 
 # MAGIC CREATE DATABASE IF NOT EXISTS conference_raw
 
 # COMMAND ----------
