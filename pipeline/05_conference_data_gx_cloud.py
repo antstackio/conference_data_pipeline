2f4305da97462b088302e7b096055e4eb5669ef5
sourav.mondal@antstack.io <sourav.mondal@antstack.io>
2023-04-12 08:01:39 +0000

    SCD-73 Add multiple expectation suite

--- pipeline/05_conference_data_gx_cloud.py
+++ pipeline/05_conference_data_gx_cloud.py
@@ -9,6 +9,7 @@
 import os
 import great_expectations as gx
 from great_expectations.core.batch import BatchRequest, RuntimeBatchRequest
+from great_expectations.core import ExpectationConfiguration
 from pyspark.dbutils import DBUtils
 
 dbutils = DBUtils()
@@ -46,21 +47,13 @@ else:
 
 # COMMAND ----------
 
-print(snowflake_connection)
-
-# COMMAND ----------
-
-print(context)
-
-# COMMAND ----------
-
 # Create a Datasource
     
 # Define a Datasource YAML Config:
 # by modifying the following YAML configuration
 
 datasource_yaml = f"""
-name: snowflake
+name: snowflake_dev
 class_name: Datasource
 execution_engine:
     class_name: SqlAlchemyExecutionEngine
@@ -69,7 +62,9 @@ data_connectors:
     default_runtime_data_connector_name:
         class_name: RuntimeDataConnector
         batch_identifiers:
-            - attendee_dim_batch
+            - event_dim_batch
+            - session_dim_batch
+            - event_registrant_dim_batch
     default_inferred_data_connector_name:
         class_name: InferredAssetSqlDataConnector
         include_schema_name: true
@@ -87,61 +82,249 @@ print(existing_datasource.config)
 
 # COMMAND ----------
 
-expectation_suite = context.create_expectation_suite(
-    expectation_suite_name="conference_attendee_dim_ex_suite"
+# MAGIC %md
+# MAGIC #### Event Expectation Suite
+
+# COMMAND ----------
+
+event_expectation_suite = context.create_expectation_suite(
+    expectation_suite_name="conference_event_dim_ex_suite_dev"
 )
 
 
 # COMMAND ----------
 
-expectation_configuration = gx.core.ExpectationConfiguration(**{
-    "expectation_type": "expect_column_values_to_not_be_null",
-    "kwargs": {
-      "column": 'state',
-    },
-    "meta":{}
-  })
-expectation_suite.add_expectation(expectation_configuration)
+expectation_configuration = ExpectationConfiguration(
+    expectation_type="expect_column_values_to_not_be_null",
+    kwargs={
+      "column": 'event_name',
+    }
+  )
+event_expectation_suite.add_expectation(expectation_configuration)
 
 
 # COMMAND ----------
 
-expectation_configuration = gx.core.ExpectationConfiguration(**{
+expectation_configuration = ExpectationConfiguration(
+    expectation_type="expect_column_values_to_not_be_null",
+    kwargs={
+      "column": 'start_date',
+    }
+  )
+event_expectation_suite.add_expectation(expectation_configuration)
+
+# COMMAND ----------
+
+expectation_configuration = ExpectationConfiguration(
+    expectation_type="expect_column_values_to_not_be_null",
+    kwargs={
+      "column": 'end_date',
+    }
+  )
+event_expectation_suite.add_expectation(expectation_configuration)
+
+# COMMAND ----------
+
+expectation_configuration = ExpectationConfiguration(
+    expectation_type="expect_column_values_to_not_be_null",
+    kwargs={
+      "column": 'zip_code',
+    }
+  )
+event_expectation_suite.add_expectation(expectation_configuration)
+
+# COMMAND ----------
+
+expectation_configuration = ExpectationConfiguration(**{
+    "expectation_type": "expect_column_values_to_be_in_set",
+    "kwargs": {
+      "column": 'country',
+      "value_set": ['us', 'can']
+    },
+    "meta":{}
+  })
+event_expectation_suite.add_expectation(expectation_configuration)
+
+
+context.save_expectation_suite(event_expectation_suite)
+
+# COMMAND ----------
+
+event_batch_request = RuntimeBatchRequest(
+    datasource_name="snowflake_dev",
+    data_connector_name="default_runtime_data_connector_name",
+    data_asset_name="conference.event_dim.asset",  # this can be anything that identifies this data
+    runtime_parameters={
+        "query": f"SELECT * from CONFORMED.EVENT_DIM"
+    },
+    batch_identifiers={"event_dim_batch": "event_dim_batch"},
+)
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC #### Session Expectation Suite
+
+# COMMAND ----------
+
+session_expectation_suite = context.create_expectation_suite(
+    expectation_suite_name="conference_session_dim_ex_suite_dev"
+)
+
+# COMMAND ----------
+
+expectation_configuration = ExpectationConfiguration(**{
+    "expectation_type": "expect_column_values_to_not_be_null",
+    "kwargs": {
+      "column": 'session_title',
+    },
+    "meta":{}
+  })
+session_expectation_suite.add_expectation(expectation_configuration)
+
+# COMMAND ----------
+
+expectation_configuration = ExpectationConfiguration(**{
+    "expectation_type": "expect_column_values_to_not_be_null",
+    "kwargs": {
+      "column": 'start_time',
+    },
+    "meta":{}
+  })
+session_expectation_suite.add_expectation(expectation_configuration)
+
+# COMMAND ----------
+
+expectation_configuration = ExpectationConfiguration(**{
+    "expectation_type": "expect_column_values_to_not_be_null",
+    "kwargs": {
+      "column": 'end_time',
+    },
+    "meta":{}
+  })
+session_expectation_suite.add_expectation(expectation_configuration)
+
+# COMMAND ----------
+
+context.save_expectation_suite(session_expectation_suite)
+
+# COMMAND ----------
+
+session_batch_request = RuntimeBatchRequest(
+    datasource_name="snowflake_dev",
+    data_connector_name="default_runtime_data_connector_name",
+    data_asset_name="conference.session_dim.asset",  # this can be anything that identifies this data
+    runtime_parameters={
+        "query": f"SELECT * from CONFORMED.SESSION_DIM"
+    },
+    batch_identifiers={"session_dim_batch": "session_dim_batch"},
+)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Event Registrants Suite
+
+# COMMAND ----------
+
+event_registrants_expectation_suite = context.create_expectation_suite(
+    expectation_suite_name="conference_event_registrants_dim_ex_suite_dev"
+)
+
+
+# COMMAND ----------
+
+expectation_configuration = ExpectationConfiguration(**{
     "expectation_type": "expect_column_values_to_not_be_null",
     "kwargs": {
       "column": 'email_address',
     },
     "meta":{}
   })
-expectation_suite.add_expectation(expectation_configuration)
-
-
-context.save_expectation_suite(expectation_suite)
+event_registrants_expectation_suite.add_expectation(expectation_configuration)
 
 # COMMAND ----------
 
-
+expectation_configuration = ExpectationConfiguration(**{
+    "expectation_type": "expect_column_values_to_not_be_null",
+    "kwargs": {
+      "column": 'registration_no',
+    },
+    "meta":{}
+  })
+event_registrants_expectation_suite.add_expectation(expectation_configuration)
 
 # COMMAND ----------
 
-run_time_batch_request = RuntimeBatchRequest(
-    datasource_name="snowflake",
+expectation_configuration = ExpectationConfiguration(**{
+    "expectation_type": "expect_column_values_to_not_be_null",
+    "kwargs": {
+      "column": 'event_name',
+    },
+    "meta":{}
+  })
+event_registrants_expectation_suite.add_expectation(expectation_configuration)
+
+# COMMAND ----------
+
+expectation_configuration = ExpectationConfiguration(**{
+    "expectation_type": "expect_column_values_to_not_be_null",
+    "kwargs": {
+      "column": 'event_id',
+    },
+    "meta":{}
+  })
+event_registrants_expectation_suite.add_expectation(expectation_configuration)
+
+# COMMAND ----------
+
+expectation_configuration = ExpectationConfiguration(**{
+    "expectation_type": "expect_column_values_to_not_be_null",
+    "kwargs": {
+      "column": 'state',
+    },
+    "meta":{}
+  })
+event_registrants_expectation_suite.add_expectation(expectation_configuration)
+
+# COMMAND ----------
+
+context.save_expectation_suite(event_registrants_expectation_suite)
+
+# COMMAND ----------
+
+event_registrant_batch_request = RuntimeBatchRequest(
+    datasource_name="snowflake_dev",
     data_connector_name="default_runtime_data_connector_name",
-    data_asset_name="conference.event_registrant.asset",  # this can be anything that identifies this data
+    data_asset_name="conference.event_registrant_dim.asset",  # this can be anything that identifies this data
     runtime_parameters={
         "query": f"SELECT * from CONFORMED.EVENT_REGISTRANT_DIM"
     },
-    batch_identifiers={"attendee_dim_batch": "attendee_dim_batch"},
+    batch_identifiers={"event_registrant_dim_batch": "event_registrant_dim_batch"},
 )
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC #### Checkpoint Definition
+
+# COMMAND ----------
+
 # First, fetch the ExpectationSuite you will use to define a Validation
-expectation_suite = context.get_expectation_suite(
-    expectation_suite_name="conference_attendee_dim_ex_suite"
+event_expectation_suite = context.get_expectation_suite(
+    expectation_suite_name="conference_event_dim_ex_suite_dev"
 )  # add your expectation suite name here
 
-checkpoint_name = "new_checkpoint"  # name your checkpoint here!
+session_expectation_suite = context.get_expectation_suite(
+    expectation_suite_name="conference_session_dim_ex_suite_dev"
+)
+
+event_registrant_expectation_suite = context.get_expectation_suite(
+    expectation_suite_name="conference_event_registrants_dim_ex_suite_dev"
+)
+
+
+checkpoint_name = "conference_data_checkpoint_v1"  # name your checkpoint here!
 
 # uncomment the lines below after successfully creating your Checkpoint to run this code again!
 # checkpoint = context.get_checkpoint(checkpoint_name)
@@ -151,9 +334,47 @@ checkpoint_config = {
     "name": checkpoint_name,
     "validations": [
         {
-            "expectation_suite_name": expectation_suite.expectation_suite_name,
-            "expectation_suite_ge_cloud_id": expectation_suite.ge_cloud_id,
-            "batch_request": run_time_batch_request,
+            "expectation_suite_name": event_expectation_suite.expectation_suite_name,
+            "expectation_suite_ge_cloud_id": event_expectation_suite.ge_cloud_id,
+            "batch_request": event_batch_request,
+            "action_list": [
+                {
+                    "name": "store_validation_result",
+                    "action": {"class_name": "StoreValidationResultAction"},
+                },
+                {
+                    "name": "store_evaluation_params",
+                    "action": {"class_name": "StoreEvaluationParametersAction"},
+                },
+                {
+                    "name": "update_data_docs",
+                    "action": {"class_name": "UpdateDataDocsAction"},
+                },
+            ],
+        },
+        {
+            "expectation_suite_name": session_expectation_suite.expectation_suite_name,
+            "expectation_suite_ge_cloud_id": session_expectation_suite.ge_cloud_id,
+            "batch_request": session_batch_request,
+            "action_list": [
+                {
+                    "name": "store_validation_result",
+                    "action": {"class_name": "StoreValidationResultAction"},
+                },
+                {
+                    "name": "store_evaluation_params",
+                    "action": {"class_name": "StoreEvaluationParametersAction"},
+                },
+                {
+                    "name": "update_data_docs",
+                    "action": {"class_name": "UpdateDataDocsAction"},
+                },
+            ],
+        },
+        {
+            "expectation_suite_name": event_registrant_expectation_suite.expectation_suite_name,
+            "expectation_suite_ge_cloud_id": event_registrant_expectation_suite.ge_cloud_id,
+            "batch_request": event_registrant_batch_request,
             "action_list": [
                 {
                     "name": "store_validation_result",
