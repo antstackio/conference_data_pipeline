9eeabce741dffdf289251a2b1f023fbbff5a2ed1
binums <binums>
2023-04-21 06:39:54 +0000

    Test: demo commit

--- tests/runner.py
+++ tests/runner.py
@@ -18,7 +18,7 @@ print(os.getcwd())
 # Skip writing pyc files on a readonly filesystem.
 sys.dont_write_bytecode = True
 
-# Run pytest.
+# Run pytest. 
 retcode = pytest.main([".", "-s", "-vv", "-p", "no:cacheprovider", "--cov","--cov-report", "term"])
 
 # Fail the cell execution if there are any test failures.
